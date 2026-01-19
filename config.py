8bedcba84d995eef5f063097b14527b3d3735984
mananjain1124 <mamanjain2005@gmail.com>
2026-01-19 22:00:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
-apikey = ""
+# config.py
+# SECURITY WARNING: Never share this file with others if it contains real keys.
+
+# 1. Get your Free Groq Key here: https://console.groq.com/keys
+groq_apikey = "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"
+
+# 2. Get your Free Weather Key here: https://home.openweathermap.org/api_keys
+weather_apikey = "be37299e80c0bc76f5ce602a95b8a5a6"
